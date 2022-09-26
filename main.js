ac2d537648f09a2dae0a9e32eb77d8af2ba4c371
Lucas <claudiolucas307@gmail.com>
2022-09-26 03:34:05 -0300

    Falta por placar e melhorar o button

--- main.js
+++ main.js
@@ -1,62 +1,100 @@
-var input = document.getElementsByClassName('jogada')
+var cellElements = document.querySelectorAll("[data-cell]")
+var winningMessage = document.getElementById("win-message")
+var top = document.getElementById("squareOne")
+var isCircleTurn = false;
+var btn = document.querySelector('button')
 
-// const square = docemnet.getElementsByClassName('square')
-var tabela = ["","",""]
-escolha = "X"
-var test = true
-
-function teste1(){ 
-    while(test == true){
-
-        console.log("OK PLS")
-        
-    
-        if(escolha == "X"){
-            document.getElementById('jogada1').innerHTML = escolha
-            tabela[0] = escolha
-            escolha = "O"
-        }
-        else if(escolha == "O"){
-            document.getElementById('jogada1').innerHTML = escolha
-            tabela[0] = escolha
-            escolha = "X"
-        }
-        
-        
-        var i = 0;
-        while( i <= 2){
-            console.log("Tabela: ", tabela[i])
-            i++
-            
-        }
-        // if (input == 'X' || input == 'O'){
-        test = false
-        // }
-    }
-    
+const changeTurns = () => {
+    isCircleTurn = !isCircleTurn 
 }
-function teste2(){ 
-    
-    console.log("OK PLS")
-    
-    if(escolha == "X"){
-        document.getElementById('jogada2').innerHTML = escolha
-        tabela[1] = escolha
-        escolha = "O"
-    }
-    else if(escolha == "O"){
-        document.getElementById('jogada2').innerHTML = escolha
-        tabela[1] = escolha
-        escolha = "X"
-    }
-    var count = 0
-
-    while( count <= 2){
-        console.log("Tabela: ", tabela[count])
-        count++
-
+// Testando todos os casos em que X ganha
+const xWin = () => {
+    if (
+        // Linhas
+        (cellElements[0].textContent == "X" && cellElements[1].textContent == "X" && cellElements[2].textContent == "X") || 
+        (cellElements[3].textContent == "X" && cellElements[4].textContent == "X" && cellElements[5].textContent == "X") || 
+        (cellElements[6].textContent == "X" && cellElements[7].textContent == "X" && cellElements[8].textContent == "X") ||
+        // Colunas
+        (cellElements[0].textContent == "X" && cellElements[3].textContent == "X" && cellElements[6].textContent == "X") ||
+        (cellElements[1].textContent == "X" && cellElements[4].textContent == "X" && cellElements[7].textContent == "X") ||
+        (cellElements[2].textContent == "X" && cellElements[5].textContent == "X" && cellElements[8].textContent == "X") ||
+        // Diagonais
+        (cellElements[0].textContent == "X" && cellElements[4].textContent == "X" && cellElements[8].textContent == "X") ||
+        (cellElements[2].textContent == "X" && cellElements[4].textContent == "X" && cellElements[6].textContent == "X") 
+    ){
+        console.log("x venceu")
+        return true
     }
 }
+
+// Testando todos os casos em que circulo ganha
+const circleWin = () => {
+    if (
+        // Linhas
+        (cellElements[0].textContent == "O" && cellElements[1].textContent == "O" && cellElements[2].textContent == "O") || 
+        (cellElements[3].textContent == "O" && cellElements[4].textContent == "O" && cellElements[5].textContent == "O") || 
+        (cellElements[6].textContent == "O" && cellElements[7].textContent == "O" && cellElements[8].textContent == "O") ||
+        // Colunas
+        (cellElements[0].textContent == "O" && cellElements[3].textContent == "O" && cellElements[6].textContent == "O") ||
+        (cellElements[1].textContent == "O" && cellElements[4].textContent == "O" && cellElements[7].textContent == "O") ||
+        (cellElements[2].textContent == "O" && cellElements[5].textContent == "O" && cellElements[8].textContent == "O") ||
+        // Diagonais
+        (cellElements[0].textContent == "O" && cellElements[4].textContent == "O" && cellElements[8].textContent == "O") ||
+        (cellElements[2].textContent == "O" && cellElements[4].textContent == "O" && cellElements[6].textContent == "O")
+    ){console.log("Circulo venceu")
+        return true
+}
+}
+
+const handleClick = (e) => {
+    // Colocar a marca (x ou circulo)
+    // o event.target possibilita sabermos quem disparou o evento
+    
+    const cell = e.target;
+    var elementToAdd = isCircleTurn ? "O" : "X";
+    cell.innerHTML= elementToAdd;
+    // Verificar pela vitória 
+
+    if(circleWin()){
+        const xMessage = document.querySelector('#circle')
+        xMessage.classList.toggle('circle')
+        winningMessage.classList.toggle('win-message-toggle')
+    }
+    if (xWin()){
+        const xMessage = document.querySelector('#x')
+        xMessage.classList.toggle('x')
+        winningMessage.classList.toggle('win-message-toggle')
+    }
+    
+    // Verificar empate
+    if(cell.textContent == "X" || cell.textContent == "O" ){
+        i = i + 1; 
+        // console.log(i)
+        if(i == 9){
+            const tieMessage = document.querySelector('#tie')
+            tieMessage.classList.toggle('tie')
+            winningMessage.classList.toggle('win-message-toggle')
+        }
+    }
+    
+    // Mudar símbolo
+    changeTurns()
+}
+var i = 0;
+for (var cell of cellElements){
+    cell.addEventListener("click", handleClick, {once: true});  
+}
+
+
+// Por enquanto coloquei um reloader
+function reloader(){
+    location.reload()
+}
+btn.addEventListener('click', reloader)
+
+
+
+
 
 
 
